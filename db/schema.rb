ca2c350942a4a806c56f2f47de668ce4cea2e98a
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-21 20:20:17 +0430

    Update the schema.rb file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_154215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.float "preparation_time"
+    t.float "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_recipes_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "recipes", "users", column: "users_id"
 end
