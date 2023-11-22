014c5e61f11612ee7af3364e50283533ec220bfc
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-22 08:24:34 +0430

    Update schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_154215) do
     t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "users_id"
-    t.index ["users_id"], name: "index_recipes_on_users_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +32,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_154215) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "recipes", "users", column: "users_id"
+  add_foreign_key "recipes", "users"
 end
