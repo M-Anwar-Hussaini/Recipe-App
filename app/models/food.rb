9181156c70b4ba6aa86ad5812c4ac1cfbd99ceb8
thasipablo <thasipablo@gmail.com>
2023-11-22 01:33:24 +0100

    add validations in models, food and recipe_food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  validates :price, numericality: { only_integer: true }
+  has_many :recipe_foods, dependent: :destroy
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_141348) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_221507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,10 +24,19 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_141348) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "recipe_foods", "foods"
 end
