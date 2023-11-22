f581dd18e9c659abc8129ed6b5cc5be90f20bd42
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-22 08:17:59 +0430

    Update the migration file.

--- db/migrate/20231121154215_add_user_id_to_recipe.rb
+++ db/migrate/20231121154215_add_user_id_to_recipe.rb
@@ -1,5 +1,5 @@
 class AddUserIdToRecipe < ActiveRecord::Migration[7.1]
   def change
-    add_reference :recipes, :users, foreign_key: true
+    add_reference :recipes, :user, foreign_key: true
   end
 end
