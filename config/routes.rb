66bac78b0bd2e7ca1f4cfd853b6378ba6a7b7de3
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-24 09:03:01 +0430

    Add resource routes to the routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'recipes#index'
+  resources :public_recipes, only: [:index]
   resources :recipes, only: [:index, :new, :create, :destroy, :show]
   resources :food, only: [:index, :new, :create, :destroy, :show]
 end
