f78b96a9130e5b6fb5bf61825a1a99f3600e17e1
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-24 09:02:38 +0430

    Add total price to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_foods, dependent: :destroy
   belongs_to :user
   validates :name, :preparation_time, :cooking_time, :description, presence: true
   validates :public, inclusion: { in: [true, false] }
@@ -6,5 +7,13 @@ class Recipe < ApplicationRecord
   def toggle_public
     self.public = !public
     save
+  end
+
+  def total_price
+    total = 0
+    recipe_foods.each do |rf|
+      total += rf.food.price
+    end
+    total
   end
 end
